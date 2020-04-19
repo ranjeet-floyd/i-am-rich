d29ab3370b13435e1040eb609afdcb0491d00fd6
ranjeet <ranjeet.floyd@gmail.com>
2020-04-19 16:35:48 +0530

    I am rich

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// This is main starting point of Flutter app.
+void main() => runApp(
+      MaterialApp(
+          home: Center(
+        child: Text("Hello Flutter World."),
+      )),
+    );
