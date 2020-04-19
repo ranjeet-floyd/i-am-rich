77fa78781be426bba1036869e076e38a62a9e9a1
ranjeet <ranjeet.floyd@gmail.com>
2020-04-19 16:59:09 +0530

    Use scaffold with Network image body in centre

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,18 @@ import 'package:flutter/material.dart';
 // This is main starting point of Flutter app.
 void main() => runApp(
       MaterialApp(
-          home: Center(
-        child: Text("Hello Flutter World."),
-      )),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text("I am Rich"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFMpR8dph6WxojWXJeZKklch9MR4UHkko-0qcjGSHkl4BlPbCW&usqp=CAU'),
+            ),
+          ),
+        ),
+      ),
     );
